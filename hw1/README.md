1ba39041fa3408f6c1ec31bec13a913df89846de
Mark Yan <b01502069@ntu.edu.tw>
2016-10-26 10:23:16 +0800

    modify run_phase1.sh

---
+++ hw1/README.md
@@ -0,0 +1,5 @@
+#Assignment 1: Word Embedding
+
+##Usage:
+`bash run_phase1.sh corpus/text8 output_phase1/`
+`bash run_phase2.sh corpus/ptt_corpus.txt output_phase2/`
--- hw1/run_phase1.sh
+++ hw1/run_phase1.sh
@@ -2,10 +2,9 @@
 # ex. bash run_phase1.sh corpus/text8 output_phase1/ 
 echo Starting hw1 phase1:
 
-#run code
+#run code and filter
 python word2vec_opt.py --train_data=$1
-python glove.py $1
-
-#filter
 python filterVocab.py fullVocab.txt < word2vec.txt > $2filter_word2vec.txt
+
+python glove.py $1
 python filterVocab.py fullVocab.txt < glove.txt > $2filter_glove.txt
