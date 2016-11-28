e250cb36120493cf7a1bc5aa2530cb2eb5e34cd6
MarkYan <b01502069@ntu.edu.tw>
2016-11-28 10:18:11 +0800

    hw3 first commit

---
+++ hw3/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+data_dir=data/ATIS_hw3
+model_dir=model_hw3
+max_sequence_length=50  # max length for train/valid/test sequence
+task=joint  # available options: intent; tagging; joint
+bidirectional_rnn=True  # available options: True; False
+
+python run_multi-task_rnn.py --data_dir $data_dir \
+      --train_dir   $model_dir\
+      --max_sequence_length $max_sequence_length \
+      --task $task \
+      --bidirectional_rnn $bidirectional_rnn
